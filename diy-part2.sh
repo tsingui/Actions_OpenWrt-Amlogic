2f78318590c0353350d8d57ce008d637d05e8fa8
tsingui <tsingui@163.com>
2023-08-16 10:46:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+# sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings    # 设置密码为空
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon）
@@ -19,12 +21,15 @@
 # sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' feeds/luci/applications/luci-app-cpufreq/Makefile
 
 # Add autocore support for armvirt
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+# sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
 # Set DISTRIB_REVISION
 # sed -i "s/OpenWrt /OpenWrt Build $(TZ=UTC-8 date "+%Y.%m.%d") Compiled By TSingui /g" package/lean/default-settings/files/zzz-default-settings
+# Set etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
 
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 10.57.20.1）
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 10.57.21.2）
 sed -i 's/192.168.1.1/10.57.21.2/g' package/base-files/files/bin/config_generate
 
 # Modify system hostname（FROM OpenWrt CHANGE TO OpenWrt-N1）
@@ -33,35 +38,36 @@ sed -i 's/192.168.1.1/10.57.21.2/g' package/base-files/files/bin/config_generate
 # Replace the default software source
 # sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
 
-sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+# sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
 
-# 拉取软件包
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# Add luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+# git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+# git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone https://github.com/kenzok8/small-package package/small-package
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 删除重复包
 # rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf package/small-package/luci-app-amlogic
-rm -rf package/small-package/luci-app-argon*
-rm -rf package/small-package/luci-theme-argon*
-rm -rf package/small-package/luci-app-koolproxyR
-rm -rf package/small-package/luci-app-godproxy
-rm -rf package/small-package/openvpn-easy-rsa-whisky
-rm -rf package/small-package/luci-app-mosdns
-rm -rf package/small-package/luci-app-openvpn-server
-rm -rf package/small-package/luci-app-store
-rm -rf package/small-package/luci-app-istorex
-rm -rf package/small-package/luci-app-quickstart
-rm -rf package/small-package/luci-app-xray
-rm -rf package/small-package/luci-app-wrtbwmon
-rm -rf package/small-package/wrtbwmon
+# rm -rf package/small-package/luci-app-amlogic
+# rm -rf package/small-package/luci-app-argon*
+# rm -rf package/small-package/luci-theme-argon*
+# rm -rf package/small-package/luci-app-koolproxyR
+# rm -rf package/small-package/luci-app-godproxy
+# rm -rf package/small-package/openvpn-easy-rsa-whisky
+# rm -rf package/small-package/luci-app-mosdns
+# rm -rf package/small-package/luci-app-openvpn-server
+# rm -rf package/small-package/luci-app-store
+# rm -rf package/small-package/luci-app-istorex
+# rm -rf package/small-package/luci-app-quickstart
+# rm -rf package/small-package/luci-app-xray
+# rm -rf package/small-package/luci-app-wrtbwmon
+# rm -rf package/small-package/wrtbwmon
 
 # 其他调整
-sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/tsingui/Actions_OpenWrt-Amlogic#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
-sed -i 's#ARMv8#openwrt_armvirt#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
-sed -i 's#opt/kernel#kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
-sed -i 's#mount -t cifs#mount.cifs#g' feeds/luci/applications/luci-app-cifs-mount/root/etc/init.d/cifs
+# sed -i 's#https://github.com/breakings/OpenWrt#https://github.com/tsingui/Actions_OpenWrt-Amlogic#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+# sed -i 's#ARMv8#openwrt_armvirt#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+# sed -i 's#opt/kernel#kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+# sed -i 's#mount -t cifs#mount.cifs#g' feeds/luci/applications/luci-app-cifs-mount/root/etc/init.d/cifs
