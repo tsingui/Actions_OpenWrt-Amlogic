7f0ba5627eb4788ac185d7aad748020e1ffc68a3
tsingui <tsingui@163.com>
2023-10-15 11:27:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,20 +13,17 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 # echo 'src-git luci-app-amlogic https://github.com/ophub/luci-app-amlogic.git;main' >> feeds.conf.default
-#aliyundrive
-echo 'src-git aliyundrive-fuse https://github.com/messense/aliyundrive-fuse.git;main' >> feeds.conf.default
-echo 'src-git aliyundrive-webdav https://github.com/messense/aliyundrive-webdav.git;main' >> feeds.conf.default
 #istore
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore.git;main' >> feeds.conf.default
 #passwall
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 #adguardhome
-echo "src-git adguardhome https://github.com/kongfl888/luci-app-adguardhome.git;master" >> feeds.conf.default
-# echo "src-git adguardhome_packages https://github.com/AdguardTeam/AdGuardHome.git;master" >> feeds.conf.default
+echo 'src-git adguardhome https://github.com/kongfl888/luci-app-adguardhome.git;master' >> feeds.conf.default
+# echo 'src-git adguardhome_packages https://github.com/AdguardTeam/AdGuardHome.git;master' >> feeds.conf.default
 
 ## 解除系统限制
 ulimit -u 10000
