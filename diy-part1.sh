2223aeac1e8a1489898c56876d2d49aad4326ce3
tsingui <tsingui@163.com>
2023-10-15 14:58:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,11 +19,8 @@ echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.
 echo 'src-git istore https://github.com/linkease/istore.git;main' >> feeds.conf.default
 #passwall
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-#adguardhome
-echo 'src-git adguardhome https://github.com/kongfl888/luci-app-adguardhome.git;master' >> feeds.conf.default
-# echo 'src-git adguardhome_packages https://github.com/AdguardTeam/AdGuardHome.git;master' >> feeds.conf.default
+# echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >> feeds.conf.default
 
 ## 解除系统限制
 ulimit -u 10000
