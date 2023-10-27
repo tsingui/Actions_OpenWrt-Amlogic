d670d0d2727ddd5618e401b894b31c9be954bcdc
tsingui <tsingui@163.com>
2023-10-27 21:53:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -41,13 +41,15 @@ sed -i 's/192.168.1.1/10.57.21.2/g' package/base-files/files/bin/config_generate
 # sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
 
 # Add software,luci-app-amlogic
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+svn co https://github.com/lisaac/luci-app-diskman/trunk/applications/luci-app-diskman package/luci-app-diskman
 git clone --depth 1 https://github.com/linkease/nas-packages-luci.git package/nas_luci
 git clone --depth 1 https://github.com/linkease/nas-packages.git package/nas
 git clone --depth 1 https://github.com/linkease/istore.git package/istore
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
-# svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/luci-app-passwall/packages
 # git clone --depth 1 https://github.com/messense/aliyundrive-fuse.git && mv aliyundrive-fuse/openwrt/* ./package && rm -rf aliyundrive-fuse
 # git clone --depth 1 https://github.com/messense/aliyundrive-webdav.git && mv aliyundrive-webdav/openwrt/* ./package && rm -rf aliyundrive-webdav
 # git clone --depth 1 https://github.com/linkease/istore.git && mv istore ./package
