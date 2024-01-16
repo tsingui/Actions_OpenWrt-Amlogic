677a1768d448dc9fcf1953164057d9fae9c46cf2
tsingui <tsingui@163.com>
2024-01-17 01:59:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -41,12 +41,15 @@ sed -i 's/192.168.1.1/10.57.21.2/g' package/base-files/files/bin/config_generate
 # sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
 
 # Add software,luci-app-amlogic
-git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 # svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-git clone --depth 1 https://github.com/ophub/luci-app-amlogic.git && mv luci-app-amlogic/luci-app-amlogic package/luci-app-amlogic
-git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 # svn co https://github.com/lisaac/luci-app-diskman/trunk/applications/luci-app-diskman package/luci-app-diskman
-git clone --depth 1 https://github.com/lisaac/luci-app-diskman.git && mv luci-app-diskman/applications/luci-app-diskman package/luci-app-diskman
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+# git clone --depth 1 https://github.com/ophub/luci-app-amlogic.git && mv luci-app-amlogic/luci-app-amlogic package/luci-app-amlogic
+git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+# git clone --depth 1 https://github.com/lisaac/luci-app-diskman.git && mv luci-app-diskman/applications/luci-app-diskman package/luci-app-diskman
+mkdir -p package/luci-app-diskman && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O package/luci-app-diskman/Makefile
+mkdir -p package/parted && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
 git clone --depth 1 https://github.com/linkease/nas-packages-luci.git package/nas_luci
 git clone --depth 1 https://github.com/linkease/nas-packages.git package/nas
 git clone --depth 1 https://github.com/linkease/istore.git package/istore
@@ -63,6 +66,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git packag
 
 # 删除重复包
 rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-diskman
 rm -rf feeds/luci/themes/luci-theme-argon
 # rm -rf feeds/luci/applications/luci-app-netdata
 # rm -rf package/small-package/luci-app-amlogic
