32708de00295c3de6a2b7a6d8c4bc0ff513b6769
tsingui <tsingui@163.com>
2024-01-17 17:26:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -65,9 +65,9 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luc
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 删除重复包
+rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 # rm -rf feeds/luci/applications/luci-app-diskman
-rm -rf feeds/luci/themes/luci-theme-argon
 # rm -rf feeds/luci/applications/luci-app-netdata
 # rm -rf package/small-package/luci-app-amlogic
 # rm -rf package/small-package/luci-app-argon*
