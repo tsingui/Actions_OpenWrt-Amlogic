f7e27c655fd7a9b9e920b124410f190172eafbb6
tsingui <tsingui@163.com>
2024-12-11 21:56:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -51,7 +51,7 @@ sed -i 's/LEDE/Tomato/g' package/base-files/luci2/bin/config_generate
 # svn co https://github.com/lisaac/luci-app-diskman/trunk/applications/luci-app-diskman package/luci-app-diskman
 # git clone --depth 1 https://github.com/lisaac/luci-app-diskman.git && mv luci-app-diskman/applications/luci-app-diskman package/luci-app-diskman && rm -rf luci-app-diskman
 # svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
-# git clone --depth 1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
+git clone --depth 1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git clone --depth 1 https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 git clone --depth 1 https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone --depth 1 https://github.com/kiddin9/openwrt-clouddrive2.git package/clouddrive2
@@ -80,7 +80,7 @@ git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git packa
 # 删除重复包
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
-# rm -rf feeds/luci/applications/luci-app-alist
+rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/luci/applications/luci-app-adguardhome
 # rm -rf feeds/luci/applications/luci-app-ddns-go
 rm -rf feeds/luci/applications/luci-app-openclash
